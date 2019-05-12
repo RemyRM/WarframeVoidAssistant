3334c5b5ba611e38486e8152f5564a69641c41e0
remy_rm <remy_rm@hotmail.nl>
2019-05-12 15:38:53 +0200

    Added voidChecker.cs?

---
+++ WarframeVoidRewardChecker/TesseractOCR.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace WarframeVoidRewardChecker
+{
+    static class TesseractOCR
+    {
+        struct CharCombi
+        {
+            public readonly char endChar;
+            public readonly char startChar;
+
+            internal CharCombi(char a, char b)
+            {
+                endChar = a;
+                startChar = b;
+            }
+        }
+        static readonly CharCombi[] charSets = new CharCombi[]
+        {
+            new CharCombi('U', 'R'),
+            new CharCombi('u', 'r'),
+            new CharCombi('O', 'R'),
+            new CharCombi('u', 'r')
+        };
+
+        /// <summary>
+        /// Read the text contents from a screenshot
+        /// </summary>
+        /// <param name="testImagePath"></param>
+        internal static void Tesseract(string testImagePath)
+        {
+            try
+            {
+
+                using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                {
+                    engine.SetVariable("tessedit_char_whitelist", "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                    using (var img = Pix.LoadFromFile(testImagePath))
+                    {
+                        using (var page = engine.Process(img))
+                        {
+                            var text = page.GetText();
+
+                            Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+                            #region checkIfNeeded
+
+                            //Console.WriteLine("Text (GetText): \r\n{0}", text);
+                            //Console.WriteLine("Text (iterator):");
+                            //using (var iter = page.GetIterator())
+                            //{
+                            //    iter.Begin();
+
+                            //    do
+                            //    {
+                            //        do
+                            //        {
+                            //            do
+                            //            {
+                            //                do
+                            //                {
+                            //                    if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                            //                    {
+                            //                        //Console.WriteLine("<BLOCK>");
+                            //                    }
+
+                            //                    //Console.Write(iter.GetText(PageIteratorLevel.Word));
+                            //                    //Console.Write(" ");
+
+                            //                    if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                            //                    {
+                            //                        //Console.WriteLine();
+                            //                    }
+                            //                } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                            //                if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                            //                {
+                            //                    //Console.WriteLine();
+                            //                }
+                            //            } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                            //        } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                            //    } while (iter.Next(PageIteratorLevel.Block));
+                            //}
+                            #endregion
+
+                            ValidateResults(ref text);
+
+                            VoidChecker.results.Add(text);
+                        }
+                    }
+                }
+                Console.WriteLine("");
+            }
+            catch (Exception e)
+            {
+                Trace.TraceError(e.ToString());
+                Debug.WriteLine("Unexpected Error: " + e.Message);
+                Debug.WriteLine("Details: ");
+                Debug.WriteLine(e.ToString());
+            }
+            //Console.Write("Press any key to continue . . . ");
+            //Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Validate if the text obtained by tesseract is valid
+        /// </summary>
+        /// <param name="input"></param>
+        static void ValidateResults(ref string input)
+        {
+            //Some character combinations end up getting recognised as whitespace, remove those whitespaces.
+            foreach (CharCombi cc in charSets)
+            {
+                //if (input.Contains("BARREL"))
+                //{
+                if (input.Contains($"{cc.endChar} {cc.startChar}"))
+                {
+                    input = input.Replace($"{cc.endChar} {cc.startChar}", $"{cc.endChar}{cc.startChar}");
+                }
+                //}
+            }
+
+            //Remove any newlines Tesseract finds
+            while (input.Contains("\n"))
+            {
+                input = input.Replace("\n", "");
+            }
+
+            if (CompareInputAgainstDictionary(input))
+            {
+                Console.WriteLine("{0} was found in the dictionary", input);
+                return;
+            }
+            else
+            {
+                Console.WriteLine("{0} was NOT found in the dictionary", input);
+                input = FindClosestMatch(input);
+                Console.WriteLine("Found {0} after closest match", input);
+            }
+        }
+
+        /// <summary>
+        /// Check if the AllPrimeItems list has an entry that matches input
+        /// </summary>
+        /// <param name="input">Item name</param>
+        /// <returns>Wether input is a valid warframe item</returns>
+        static bool CompareInputAgainstDictionary(string input)
+        {
+            List<WarframeMarketItemClass> itemClass = WarframeMarketApi.GetAllPrimeItems();
+
+            return itemClass.Exists(o => o.item_name.Equals(input));
+        }
+
+        /// <summary>
+        /// Sees if the first word in combination with any other words appears in the total items list, if this is the case we know it's that item
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        static string FindClosestMatch(string input)
+        {
+            //Formas arn't in the item list, but should still be recognised.
+            if (input.Contains("FORMA"))
+            {
+                return "FORMA BLUEPRINT";
+            }
+
+            List<WarframeMarketItemClass> itemClass = WarframeMarketApi.GetAllPrimeItems();
+
+            string[] splitInput = input.Split(null);
+            WarframeMarketItemClass matchFirstWord = itemClass.FirstOrDefault(o => o.item_name.Contains(splitInput[0]));
+
+            if (matchFirstWord != null)
+            {
+                Console.WriteLine("First word matched any word in total word list. Continueing");
+            }
+            else
+            {
+                Console.WriteLine("Error, no word matched {0}", splitInput[0]);
+                //TODO: implement something that searches for the nearest word
+                return "No match found";
+            }
+
+            //Make a list of lists containing the items, this so we can sort them by the index given by split input. This way we can see if any item is found twice, meaning we got a match.
+            List<List<WarframeMarketItemClass>> occuranceOfWordList = new List<List<WarframeMarketItemClass>>();
+
+            //Loop through all the items found with the keywords found in SplitInput. Add all the results found to matchesWord
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                List<WarframeMarketItemClass> matchesWord = itemClass.Where(o => o.item_name.Contains(splitInput[i])).ToList();
+                occuranceOfWordList.Add(matchesWord);
+            }
+
+            //If any of the results found with the first keyword is also found in the second or third list we got a match, and we know it's that item.
+            WarframeMarketItemClass result = null;
+            for (int i = 1; i <= splitInput.Length; i++)
+            {
+                try
+                {
+                    if (result == null)
+                    {
+                        result = occuranceOfWordList[0].Intersect(occuranceOfWordList[i]).FirstOrDefault();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return result.item_name;
+        }
+    }
+}
+
---
+++ WarframeVoidRewardChecker/VoidChecker.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
+using Tesseract;
+using System.Collections.Generic;
+
+namespace WarframeVoidRewardChecker
+{
+    public partial class VoidChecker : Form
+    {
+        #region DllImports
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, ref RECT lpRect);
+        #endregion
+
+        #region structs
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        #endregion
+
+        static RECT rect;
+
+        static Process warframeProcess;
+        static IntPtr windowHandle;
+
+        static int windowHeight;
+        static int windowWidth;
+        static int yOffset;
+        static int[] xOffsets;
+
+        internal static List<string> results = new List<string>();
+
+        /// <summary>
+        /// Setup the screenshot area
+        /// </summary>
+        public VoidChecker()
+        {
+            StartPosition = FormStartPosition.CenterScreen;
+            InitializeComponent();
+            Init();
+
+            Size = new Size(rect.Right - rect.Left, rect.Bottom - rect.Top);
+
+            FormBorderStyle = FormBorderStyle.None;
+            BackColor = Color.LimeGreen;
+            TransparencyKey = Color.LimeGreen;
+
+            Thread t = new Thread(InputLoop);
+            t.Start();
+        }
+
+        /// <summary>
+        /// Initialisation
+        /// </summary>
+        static void Init()
+        {
+            GetWarframeProcess();
+
+            rect = new RECT();
+            GetWindowRect(windowHandle, ref rect);
+
+            windowHeight = rect.Bottom - rect.Top;
+            windowWidth = rect.Right - rect.Left;
+            yOffset = (int)Math.Floor(windowHeight / 2.355);
+
+            xOffsets = new int[]
+            {
+                (int)Math.Floor(windowWidth / 18.0),
+                (int)Math.Floor(windowWidth / 3.55),
+                (int)Math.Floor(windowWidth / 1.98),
+                (int)Math.Floor(windowWidth / 1.37)
+            };
+
+            Debug.WriteLine("Left: " + rect.Left + " Right: " + rect.Right + " top: " + rect.Top + " bottom: " + rect.Bottom);
+            Debug.WriteLine("width: " + windowWidth);
+            Debug.WriteLine("height:" + windowHeight);
+        }
+
+        /// <summary>
+        /// Get the warframe process
+        /// </summary>
+        static int iter = 0;
+        static void GetWarframeProcess()
+        {
+            try
+            {
+                Process[] processes = Process.GetProcessesByName("Warframe.x64");
+
+                warframeProcess = processes.FirstOrDefault();
+                if (warframeProcess != null)
+                {
+                    windowHandle = warframeProcess.MainWindowHandle;
+                    Debug.WriteLine("Warframe was found");
+                }
+                else
+                {
+                    if (iter > 10)
+                    {
+                        //start new warframe process
+                    }
+                    Debug.WriteLine("Warframe was not found, retrying");
+                    iter++;
+                    GetWarframeProcess();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Error: " + e);
+            }
+        }
+
+        /// <summary>
+        /// Main loop that waits for a keypress
+        /// </summary>
+        static void InputLoop()
+        {
+            Console.WriteLine("Input loop is ready");
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.F10) < 0)
+                {
+                    foreach (int x in xOffsets)
+                    {
+                        TakeScreenShot(x, yOffset);
+                    }
+
+                    foreach (string s in results)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    //Thread.Sleep(2000);
+                    Console.WriteLine("-------------------------");
+                    results.Clear();
+                    //Console.ReadLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Takes a screenshot of the current process window offset by x and y
+        /// </summary>
+        /// <param name="x">Location offset along the x axis</param>
+        /// <param name="y">Location offset along the y axis</param>
+        static void TakeScreenShot(int x, int y)
+        {
+
+            //Create a new bitmap.
+            var bmpScreenshot = new Bitmap(400, 30, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            //Create a graphics object from the bitmap.
+            var gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+            //Copy the pixels from the screen into the Graphics
+            gfxScreenshot.CopyFromScreen(x, y, 0, 0, new Size(rect.Right - rect.Left + 20, rect.Bottom - rect.Top), CopyPixelOperation.SourceCopy);
+
+            Random rand = new Random();
+            int randNum = rand.Next(0, 10000);
+            string path = @"D:\Dev\C#\WarframeVoidRewardChecker\WarframeVoidRewardChecker\Resources\Images\Screenshot_" + randNum + ".png";
+
+            bmpScreenshot = ToGrayScale(bmpScreenshot);
+            bmpScreenshot = RemoveBackground(bmpScreenshot);
+
+            bmpScreenshot.Save(path, System.Drawing.Imaging.ImageFormat.Png);
+
+            TesseractOCR.Tesseract(path);
+        }
+
+        /// <summary>
+        /// Removes any pixels of which the blue value is darker than 30. Since it's a grayscale image this is a constant darkness.
+        /// </summary>
+        /// <param name="bmp"></param>
+        /// <returns></returns>
+        private static Bitmap RemoveBackground(Bitmap bmp)
+        {
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color c = bmp.GetPixel(x, y);
+                    if (c.B < 30)
+                    {
+                        bmp.SetPixel(x, y, Color.FromArgb(0, 0, 0, 0));
+                    }
+                }
+            }
+
+            return bmp;
+        }
+
+        /// <summary>
+        /// Create a grayscale version of an image
+        /// </summary>
+        /// <param name="Bmp"></param>
+        /// <returns></returns>
+        static Bitmap ToGrayScale(Bitmap Bmp)
+        {
+            int rgb;
+            Color c;
+
+            for (int y = 0; y < Bmp.Height; y++)
+            {
+                for (int x = 0; x < Bmp.Width; x++)
+                {
+                    c = Bmp.GetPixel(x, y);
+                    rgb = (int)Math.Round(.299 * c.R + .587 * c.G + .114 * c.B);
+                    Bmp.SetPixel(x, y, Color.FromArgb(rgb, rgb, rgb));
+                }
+            }
+            return Bmp;
+        }
+    }
+}
